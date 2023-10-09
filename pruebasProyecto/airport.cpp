3cc25ff33b9c7cf7652f51766113c792a8adb716
garizaojuan2 <124204322+garizaojuan2@users.noreply.github.com>
2023-10-08 22:41:16 -0500

    funcionalidad comprar vuelo

--- pruebasProyecto/airport.cpp
+++ pruebasProyecto/airport.cpp
@@ -10,3 +10,18 @@ Airport& Airport::getInstance() {
 void Airport::assignFlight(Aircraft &aircrft, Flight &flight, Gate &gate){
   
 }
+
+void Airport:: buyFlight(string destination, string date){
+    pair<string, string> infoRequiered(destination, date);
+    map<pair<string, string>, Flight>::iterator it = flightsRegister.find(infoRequiered);
+    if(it != flightsRegister.end()){
+        if((*it).getAvailableSeats() <= 0)
+            cout << "El vuelo para el destino y fecha seleccionada, no se encuentra disponible" << endl;
+        else{
+            cout << "Su reserva ha sido exitosa" << endl;
+            (*it).setAvailableSeats((*it).getAvailableSeats() - 1);
+        }
+        }
+    else
+        cout << "El vuelo no está disponible" << endl;
+}
