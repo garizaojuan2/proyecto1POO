ebcb68eab6eaacd037f868b11db327de2459e025
garizaojuan2 <124204322+garizaojuan2@users.noreply.github.com>
2023-11-14 11:39:45 -0500

    controller comments

--- pythonPr/controlTower.py
+++ pythonPr/controlTower.py
@@ -1,20 +1,45 @@
 from modeloDeRevista import TodoModel
+
 class ControlTower:
     def __init__(self):
+        """
+        Constructor de la clase ControlTower.
+
+        Inicializa la torre de control con listas vacías de aeronaves, puertas de embarque y utiliza
+        una instancia del modelo TodoModel para gestionar los datos del aeropuerto.
+        """
         self.aircrafts = []
         self.gates = []
         self.model = TodoModel()
 
     @classmethod
     def get_instance(cls):
+        """
+        Método de clase para obtener una instancia única de la clase ControlTower mediante el patrón singleton.
+
+        Retorna:
+        - ControlTower: Instancia única de la clase ControlTower.
+        """
         if not hasattr(cls, 'instance'):
             cls.instance = ControlTower()
         return cls.instance
 
     def add_airplane(self, aircraft):
+        """
+        Agrega una aeronave a la lista de aeronaves controladas por la torre.
+
+        Parámetros:
+        - aircraft (Airplane): Objeto Airplane que representa la aeronave a agregar.
+        """
         self.aircrafts.append(aircraft)
 
     def aircraft_take_off(self, aircraft):
+        """
+        Simula el despegue de una aeronave y notifica a otras aeronaves.
+
+        Parámetros:
+        - aircraft (Airplane): Objeto Airplane que representa la aeronave que despega.
+        """
         msg = f"El avión {aircraft.get_ident()} ha despegado."
         print(f"Torre de Control: {msg}")
         self.notify(aircraft, msg)
@@ -25,21 +50,47 @@ class ControlTower:
                 break
 
     def aircraft_landed(self, aircraft):
+        """
+        Simula el aterrizaje de una aeronave y notifica a otras aeronaves.
+
+        Parámetros:
+        - aircraft (Airplane): Objeto Airplane que representa la aeronave que aterriza.
+        """
         msg = f"El avión {aircraft.get_ident()} ha aterrizado."
         print(f"Torre de Control: {msg}")
         self.notify(aircraft, msg)
 
-    def aircraft_in_flight(self, aircraft, ubicacion):
-        msg = f"El avión {aircraft.get_ident()} informa su ubicación: {ubicacion}"
+    def aircraft_in_flight(self, aircraft, location):
+        """
+        Informa la ubicación de una aeronave en vuelo y notifica a otras aeronaves.
+
+        Parámetros:
+        - aircraft (Airplane): Objeto Airplane que representa la aeronave en vuelo.
+        - location (str): Ubicación actual de la aeronave.
+        """
+        msg = f"El avión {aircraft.get_ident()} informa su ubicación: {location}"
         print(f"Torre de Control: {msg}")
         self.notify(aircraft, msg)
 
     def notify(self, aircraft, msg):
+        """
+        Notifica a otras aeronaves sobre un mensaje específico.
+
+        Parámetros:
+        - aircraft (Airplane): Objeto Airplane que envía la notificación.
+        - msg (str): Mensaje a enviar.
+        """
         for other_aircraft in self.aircrafts:
             if other_aircraft != aircraft:
                 other_aircraft.receive_info(msg)
 
     def assign_boarding_gate(self, flight):
+        """
+        Asigna una puerta de embarque a un vuelo disponible.
+
+        Parámetros:
+        - flight (Flight): Objeto Flight que representa el vuelo al cual se le asignará la puerta de embarque.
+        """
         available_gate = None
         l = self.model.getAllPuertaDeEmbarque()
         for gate in l:
