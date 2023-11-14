2ab3f329055f4e1ece123dc22695cbf6b7557bc0
garizaojuan2 <124204322+garizaojuan2@users.noreply.github.com>
2023-11-14 11:28:27 -0500

    comments airline

--- pythonPr/airline.py
+++ pythonPr/airline.py
@@ -1,21 +1,40 @@
 
 from airport import Airport
+
 class Airline:
-    def __init__(self, i, n):
-        self.name = n
-        self.ident = i
+    def __init__(self, airline_id, name):
+        """
+        Constructor de la clase Airline.
+
+        Parámetros:
+        - airline_id (str): Identificación única de la aerolínea.
+        - name (str): Nombre de la aerolínea.
+        """
+        self.ident = airline_id
+        self.name = name
         self.flights = []
 
-    def getName(self):
+    def get_name(self):
+        """
+        Obtiene el nombre de la aerolínea.
+
+        Retorna:
+        - str: Nombre de la aerolínea.
+        """
         return self.name
+
     def add_flight(self, flight):
+        """
+        Agrega un vuelo a la lista de vuelos de la aerolínea y lo asigna a un avión en el aeropuerto.
+
+        Parámetros:
+        - flight (Flight): Objeto Flight que representa el vuelo a agregar.
+        """
         self.flights.append(flight)
-        aeropuerto = Airport.get_instance()
-        aeropuerto.assign_flight(flight)
+        airport = Airport.get_instance()
+        airport.assign_flight(flight)
+
         
-    def buyFlight(self):
-        hola = 0
-    
 
         
 
